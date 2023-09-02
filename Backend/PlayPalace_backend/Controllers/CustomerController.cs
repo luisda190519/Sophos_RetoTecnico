c8d2fee44300bac51962e967cd3b2bb11b35a824
luis.fuentes <licerol@uninorte.edu.co>
2023-09-02 15:39:01 -0500

    Some controller added

---
+++ Backend/PlayPalace_backend/Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayPalace_backend.Context;
+using PlayPalace_backend.Models;
+
+namespace PlayPalace_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public readonly ProjectContext dbContext;
+
+        public CustomerController(ProjectContext context)
+        {
+            dbContext = context;
+        }
+
+        //Create a new user
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateCustomer([FromBody] Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest("Customer data is invalid.");
+            }
+
+            try
+            {
+                dbContext.Customers.Add(customer);
+                await dbContext.SaveChangesAsync();
+
+                return CreatedAtAction("GetCustomerById", new { id = customer.CustomerID }, customer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error creating the customer: {ex.Message}");
+            }
+        }
+
+        //Get customer by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        {
+            var customer = await dbContext.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound("Customer not found.");
+            }
+
+            return Ok(customer);
+        }
+
+
+        //get customer info
+        [HttpGet("{id}/details")]
+        public async Task<ActionResult> GetCustomerDetails(int id)
+        {
+            var customer = await dbContext.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound("Customer not found.");
+            }
+
+            var customerDetails = await dbContext.Customers
+                .Where(c => c.CustomerID == id)
+                .Select(c => new
+                {
+                    Customer = c,
+                    Balance = dbContext.Rentals
+                        .Where(r => r.CustomerID == id)
+                        .Sum(r => r.Price),
+                    DeliveryDate = dbContext.Rentals
+                        .Where(r => r.CustomerID == id)
+                        .Max(r => r.DueDate),
+                    RentedTitles = dbContext.Rentals
+                        .Where(r => r.CustomerID == id)
+                        .Select(r => r.Game.Title)
+                        .ToList()
+                })
+                .FirstOrDefaultAsync();
+
+            if (customerDetails == null)
+            {
+                return NotFound("Customer details not found.");
+            }
+
+            return Ok(customerDetails);
+        }
+
+
+
+
+
+    }
+}
---
+++ Backend/PlayPalace_backend/Controllers/GamesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayPalace_backend.Context;
+using PlayPalace_backend.Models;
+
+
+namespace PlayPalace_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        public readonly ProjectContext dbContext;
+
+        public GamesController(ProjectContext context)
+        {
+            dbContext = context;
+        }
+
+        //List all the games in the database
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        {
+            var games = await dbContext.Games.ToListAsync();
+            return Ok(games);
+        }
+
+
+        //List games based on the director
+        [HttpGet("director/{director}")]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGamesByDirector(string director)
+        {
+            var games = await dbContext.Games
+                .Where(g => g.Director == director)
+                .ToListAsync();
+
+            return Ok(games);
+        }
+
+
+        //List games based the main character
+        [HttpGet("protagonists/{protagonists}")]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGamesByProtagonists(string protagonists)
+        {
+            var games = await dbContext.Games
+                .Where(g => g.MainCharacters.Any(mc => mc.Name.Contains(protagonists)))
+                .ToListAsync();
+
+            return Ok(games);
+        }
+
+        //List games by producer or brand
+        [HttpGet("producerOrBrand/{producer}")]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGamesByProducer(string query)
+        {
+            var games = await dbContext.Games
+                .Where(g => g.Producer == query || g.Brand.Name == query)
+                .ToListAsync();
+
+            return Ok(games);
+        }
+
+
+        //List games by release date
+        [HttpGet("release/{year}")]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGamesByReleaseDate(int year)
+        {
+            var games = await dbContext.Games
+                .Where(g => g.Year.Year == year)
+                .ToListAsync();
+
+            return Ok(games);
+        }
+
+        //Create a game
+        [HttpPost]
+        public async Task<ActionResult<Game>> CreateGame([FromBody] Game game)
+        {
+            if (game == null)
+            {
+                return BadRequest("Game data is invalid.");
+            }
+
+            try
+            {
+                dbContext.Games.Add(game);
+                await dbContext.SaveChangesAsync();
+
+                return CreatedAtAction("GetGameById", new { id = game.GameID }, game);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error creating the game: {ex.Message}");
+            }
+        }
+
+
+    }
+}
