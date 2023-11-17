7f4aaa5852bc12bb845ecc83fac8ac27ba16a45f
luis.fuentes <licerol@uninorte.edu.co>
2023-11-17 10:44:20 -0500

    Final

--- Frontend/src/Components/AddGame.jsx
+++ Frontend/src/Components/AddGame.jsx
@@ -30,17 +30,17 @@ function AddGame({ setScreen }) {
         let newValue = e.target.value;
         newValue = newValue.split(",");
 
-        if (!selectedPlataformsId.includes(newValue)) {
+        if (!selectedPlataformsId.includes(newValue[0])) {
             setSelectedPlataformsId([...selectedPlataformsId, newValue[0]]);
             setSelectedPlataformsName([...selectedPlataformsName, newValue[1]]);
         }
     };
 
     // Handle platform deletion
-    const deletePlatform = (e, name) => {
+    const deletePlatform = (e, id) => {
         e.preventDefault();
 
-        const indexToRemove = selectedPlataformsName.indexOf(name);
+        const indexToRemove = selectedPlataformsId.indexOf(id);
 
         if (indexToRemove !== -1) {
             const newSelectedPlataformsId = [...selectedPlataformsId];
@@ -57,13 +57,13 @@ function AddGame({ setScreen }) {
         e.preventDefault();
 
         const newGame = {
-            Title: title,
-            Year: year,
-            Director: director,
-            Producer: producer,
-            ImageUrl: imageUrl,
-            Price: parseFloat(price),
-            Description: description,
+            title: title,
+            year: year,
+            director: director,
+            producer: producer,
+            imageUrl: imageUrl,
+            price: parseFloat(price),
+            description: description,
             platformIds: selectedPlataformsId,
         };
 
@@ -85,7 +85,7 @@ function AddGame({ setScreen }) {
     // Trigger the getPlatforms function when the component mounts
     useEffect(() => {
         getPlatforms();
-    }, [platforms]);
+    }, []); // Removed [platforms] to prevent infinite loop
 
     return (
         <div>
@@ -97,7 +97,61 @@ function AddGame({ setScreen }) {
             </button>
             <form className="text-white">
                 {/* Input fields for game details */}
-                {/* ... (omitted for brevity) ... */}
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Titulo</label>
+                    <input type="text" className="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="year" className="form-label">Año de lanzamiento</label>
+                    <input type="number" className="form-control" id="year" value={year} onChange={(e) => setYear(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="director" className="form-label">Director</label>
+                    <input type="text" className="form-control" id="director" value={director} onChange={(e) => setDirector(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="producer" className="form-label">Productor</label>
+                    <input type="text" className="form-control" id="producer" value={producer} onChange={(e) => setProducer(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="imageUrl" className="form-label">URL de la imagen</label>
+                    <input type="text" className="form-control" id="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="price" className="form-label">Precio de alquiler</label>
+                    <input type="number" className="form-control" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Descripcion</label>
+                    <textarea className="form-control" id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                </div>
+                {/* Add similar input fields for other details */}
+
+                {/* Platform selection */}
+                <div className="mb-3">
+                    <label htmlFor="platforms" className="form-label">Select Platforms</label>
+                    <select
+                        className="form-select"
+                        onChange={handlePlatformChange}
+                        value=""
+                    >
+                        <option value="" disabled>Select platform...</option>
+                        {platforms.map((platform) => (
+                            <option key={platform.platformID} value={`${platform.platformID},${platform.name}`}>
+                                {icons[platform.name]} {platform.name}
+                            </option>
+                        ))}
+                    </select>
+                    {/* Display selected platforms */}
+                    <div className="mt-3">
+                        {selectedPlataformsName.map((name, index) => (
+                            <span key={index} className="badge bg-secondary me-2">
+                                {name} <span className="click" onClick={(e) => deletePlatform(e, selectedPlataformsId[index])}>x</span>
+                            </span>
+                        ))}
+                    </div>
+                </div>
+
                 <button
                     type="submit"
                     className="btn btn-primary"
